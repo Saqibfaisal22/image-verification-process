771309625e61818b36d3c545960934a0da63d5d2
Fardeen15 <fardeenmemon17@gmail.com>
2025-09-03 02:41:57 +0500

    admin cred

--- src/firebase/admin.js
+++ src/firebase/admin.js
@@ -1,22 +1,22 @@
 import admin from 'firebase-admin';
-import serviceAccount from '../../keys.json'
+// import serviceAccount from '../../keys.json'
 // if (!admin.apps.length) {
 
 // var serviceAccount = require("path/to/serviceAccountKey.json");
 
-console.log(process.env.FIREBASE_PRIVATE_KEY)
+// console.log(process.env.FIREBASE_PRIVATE_KEY)
 try {
-    admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
-      databaseURL: "https://image-verification-process-default-rtdb.firebaseio.com"
-    });
     // admin.initializeApp({
-    //   credential: admin.credential.cert({
-    //     projectId: process.env.FIREBASE_PROJECT_ID,
-    //     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    //     privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    //   }),
+    //   credential: admin.credential.cert(serviceAccount),
+    //   databaseURL: "https://image-verification-process-default-rtdb.firebaseio.com"
     // });
+    admin.initializeApp({
+      credential: admin.credential.cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      }),
+    });
     // console.log("Firebase Admin SDK initialized successfully");
   } catch (error) {
     console.error('Firebase admin initialization error', error.stack);
